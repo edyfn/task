1bf2cc032a907afb0427891f7fb2fa7cf2300b76
edyfn <edirleyfranca@gmail.com>
2021-06-28 19:10:10 -0300

    Add Class Task

---
+++ src/main/java/com/example/task/Task.java
@@ -0,0 +1,25 @@
+package com.example.task;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    @NotNull(message = "{NotNull.Task.name}")
+    private String name;
+
+    @Column
+    @NotNull(message = "{NotNull.Task.description}")
+    private String description;
+
+}
